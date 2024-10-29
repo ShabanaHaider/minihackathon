9263394c650114b245d76fc2ac044eee935b30e3
ShabanaHaider <159899692+ShabanaHaider@users.noreply.github.com>
2024-10-30 00:18:37 +0500

    Update page.tsx

--- app/toyotacorolla/page.tsx
+++ app/toyotacorolla/page.tsx
@@ -1,10 +1,10 @@
-
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Car() {
   return (
     <div className="flex justify-center mt-20 items-center">
-      <img
+      <Image
         src="./1-toyotacorolla.png"
         alt="Toyota Corolla"
         className="w-200 h-200" // Updated to a valid Tailwind size class
